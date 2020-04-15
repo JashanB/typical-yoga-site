f3ecda50dd58b732437697fa399964d0d9c24748
JashanB <jashan_brar50@hotmail.com>
2020-04-15 16:14:46 -0400

    added seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- -- Users table seeds here (Example)
 -- INSERT INTO users (name) VALUES ('Alice');
 -- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO employees (name) VALUES ('Alice');
+INSERT INTO employees (name) VALUES ('Kira');
+INSERT INTO employees (name) VALUES ('Bob');
---
+++ db/seeds/02_classes.sql
@@ -0,0 +1,7 @@
+-- Widgets table seeds here (Example)
+-- INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
+-- INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
+-- INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+INSERT INTO classes (employee_id, duration, date, isFull) VALUES (1, 3, '2020-04-19', false);
+INSERT INTO classes (employee_id, duration, date, isFull) VALUES (1, 2.5, '2020-04-20', true);
+INSERT INTO classes (employee_id, duration, date, isFull) VALUES (1, 1, '2020-04-21', false);
